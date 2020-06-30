c61efe0cd20acd09f03dfac32b3da8863bbff666
Frankovskyyy <62710663+Frankovskyyy@users.noreply.github.com>
2020-06-30 10:11:50 +0200

    DetailsPage - poprawki

--- lib/screens/detailsPage.dart
+++ lib/screens/detailsPage.dart
@@ -23,9 +23,15 @@ class DetailsPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          DetailsCard(),
-          DetailsCard(),
-          DetailsCard(),
+          DetailsCard(
+              color: Colors.white,
+              buttonText: 'Udostępnij przeysyłkę do obserwowania'),
+          DetailsCard(
+              color: Colors.grey[300],
+              buttonText: 'Włącz radar by nie ominąć przesyłki'),
+          DetailsCard(
+              color: Colors.white,
+              buttonText: 'Udostępnij przeysyłkę do obserwowania'),
         ],
       ),
     );
@@ -33,17 +39,16 @@ class DetailsPage extends StatelessWidget {
 }
 
 class DetailsCard extends StatelessWidget {
-  const DetailsCard({
-    Key key,
-  }) : super(key: key);
-
+  final color;
+  final buttonText;
+  DetailsCard({@required this.color, @required this.buttonText});
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
       width: double.infinity,
       height: 700.h,
-      color: Colors.white,
+      color: color,
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -120,15 +125,15 @@ class DetailsCard extends StatelessWidget {
               height: MediaQuery.of(context).size.height * 0.05,
               width: MediaQuery.of(context).size.width * 0.9,
               child: OutlineButton(
+                color: Colors.white,
                 disabledBorderColor: Colors.yellow[700],
                 borderSide: BorderSide(
                     color: Colors.yellow[700],
                     style: BorderStyle.solid,
                     width: 5.w),
                 onPressed: null,
-                color: Colors.purple,
                 child: Text(
-                  'Udostępnij przesyłkę do obserwowania',
+                  buttonText,
                   style: BlackBoldStyle.copyWith(
                       fontSize:
                           ScreenUtil().setSp(27, allowFontScalingSelf: true)),
